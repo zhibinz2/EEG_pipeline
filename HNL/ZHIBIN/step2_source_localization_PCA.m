e62a9cfcf4de268ad0d96841c374740282a465c8
ZHIBIN ZHOU <56317282+zhibinz2@users.noreply.github.com>
2024-04-06 22:20:16 -0700

    Create step2_source_localization_PCA.m

---
+++ HNL/ZHIBIN/step2_source_localization_PCA.m
@@ -0,0 +1,7 @@
+% This is for my own reference: my earlier unorganized code can be found at:
+% open /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization/organize_62stroke/step1_clean_data.m
+% Created by Zhibin 4/6/2024
+% #####################################################################################################################
+
+%% source localization and pca to get cortical source data
+% load forward matrix and source
