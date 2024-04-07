58b88476e1c44ddd5ec659720d63b80041a08366
ZHIBIN ZHOU <zhibinz2@uci.edu>
2024-04-07 12:50:15 -0700

    MNE

--- HNL/ZHIBIN/step2_source_localization_PCA.m
+++ HNL/ZHIBIN/step2_source_localization_PCA.m
@@ -9,31 +9,37 @@
 %% Source localization 
 % load forward matrix and source
 clear
-load('./base_files/brain_source.mat')
+load('./base_files/brain_source.mat');
+
 
 % roiNames_250: labels for 463 ROIs (448 cortical + 15 subcortical ROIs)
 % scale250_subcortROIs: indices for 15 subcortical ROIs in roiNames_250
-% corti_ave_source_coor: coordinates for 448 cortical ROIs
-% corti_ave_source_labl: indices for 448 cortical ROIs in roiNames_250
-% corti_roiNames: names of the 448 cortical ROIs
 % leadfield: leadfield matrix from MNE (256 EEG channels X 5124 brain sources)
 % source_rr: coordinates of the 5124 brain sources
 
+% corti_ave_source_coor: coordinates for 448 cortical ROIs
+% corti_ave_source_labl: indices for 448 cortical ROIs in roiNames_250
+% corti_roiNames: names of the 448 cortical ROIs
+
+% source_labels: indices for the 5124 brain sources in roiNames_250
+
 % Brain: no need, delete from mat?
 % parcels: contain labels in the brain, no need delete from mat?
+clear brain parcels
 
 % get inverse matrix for source localization
 % You will to add the following repo to your path:
 % https://github.com/wodeyara/AdaptiveGraphicalLassoforParCoh/tree/main/Simulations/util
-addpath ../../AdaptiveGraphicalLassoforParCoh/Simulations/util % this is my local path
+addpath ../../../AdaptiveGraphicalLassoforParCoh/Simulations/util % this is my local path
 [inversemat] = inversemodel(leadfield,'prctile',1);
 
 
 subj_files=[0:1:60];
-f=3; % select one patient (f=1:61)
+f=3; % select one patient to run source localization (f=1:61)
 
 % Navigate to your cleaned preprocessed EEG data directory:
-% cd /home/zhibinz2/Documents/GitHub/archive/EEG_stroke_62_cleaned/
+% cd /home/zhibinz2/Documents/GitHub/archive/EEG_stroke_62_cleaned
+% cd /ssd/zhibin/archive/EEG_stroke_62_cleaned
 % load the preprocessed EEG file for one patient
 load([num2str(subj_files(f)) '.mat'],'preprocessed_eeg','subject_ID', ...
     'chanlocs','ch_labels','ch_dubious','ch_peripheral','Fs');
@@ -44,19 +50,19 @@ source_data=inversemat*preprocessed_eeg;
 
 %% PCA to aggregate cortical source data
 % PCA to aggregate source data in each ROI
-fra_eigenvalues=zeros(1,max(unique(source_labels))); % no need to include
+% fra_eigenvalues=zeros(1,max(unique(source_labels))); % no need to include
 corti_source_data=[];
 for sr=1:max(unique(source_labels))
     I=find(source_labels==sr); % need to load save and load this source_labels
     if ~isempty(I)
         [~, SCORE, LATENT] = pca(source_data(I,:)','Centered',false);
-        fra_eigenvalues(sr)=LATENT(1)/sum(LATENT); % weight of 1st eigenvalue
+        % fra_eigenvalues(sr)=LATENT(1)/sum(LATENT); % weight of 1st eigenvalue
         corti_source_data=[corti_source_data SCORE(:,1)];
     end
 end % 66s
 
 % remove the non-zeros fraction of eigenvalues and subcortical rois
-corti_fra_eigenvalues=fra_eigenvalues(ave_source_label); % no need to include
+% corti_fra_eigenvalues=fra_eigenvalues(ave_source_label); % no need to include
 % need to load save and load this ave_source_label
     
 % remove subcortical roi
