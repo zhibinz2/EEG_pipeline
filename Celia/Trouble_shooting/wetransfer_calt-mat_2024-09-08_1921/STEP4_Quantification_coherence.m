816d512a94e7ae3ab3fe0f6ec845a5423dc164b3
ZHIBIN ZHOU <zhibinz2@uci.edu>
2024-09-13 10:15:45 -0700

    debugForCelia

--- Celia/Trouble_shooting/wetransfer_calt-mat_2024-09-08_1921/STEP4_Quantification_coherence.m
+++ Celia/Trouble_shooting/wetransfer_calt-mat_2024-09-08_1921/STEP4_Quantification_coherence.m
@@ -1,6 +1,6 @@
 clear
-DOSSIERS={'E:\DATASET\4_Source_reconstructed\Control','E:\DATASET\4_Source_reconstructed\patient'};
-dossiers={'Control','Patient'};
+% DOSSIERS={'E:\DATASET\4_Source_reconstructed\Control','E:\DATASET\4_Source_reconstructed\patient'};
+% dossiers={'Control','Patient'};
 % HNL Method
 Fs=500;
 srnew = 200;
@@ -9,8 +9,8 @@ downsample = Fs/srnew;
 passbands = [1 4;4.5 8;8.5 12; 12.5 30; 30.5 50];
 bandlabels = {'Delta','Theta', 'Alpha', 'Beta', 'Gamma'};
 
-load('E:\DATASET\Script\Individual_Connectome.mat');
-ii=0;
+% load('E:\DATASET\Script\Individual_Connectome.mat');
+% ii=0;
 % make filter
 attenuation=60;
 filt_ds=cell(1,length(passbands));
@@ -25,22 +25,48 @@ for freqBand=1:length(bandlabels)
     filt_ds{freqBand}=filt_d;
 end
 
-for d=1%:1:length(DOSSIERS)
 
-    dossier1=DOSSIERS{1,d};
-    F=dir(fullfile(dossier1,'*.mat'));
+% AGL variables
+% https://github.com/wodeyara/AdaptiveGraphicalLassoforParCoh
+% addpath(genpath('/home/zhibinz2/Documents/GitHub/AdaptiveGraphicalLassoforParCoh'))
+allLambdas = fliplr([.6,.5,.4,.3,.2,.175,.15,.125, .1, .075, .05, .025, .01]);
+allLambdasOut = fliplr([.6,.5,.4,.3,.2,.175,.15,.125, .1, .075, .05, .025, .01]);
+n_Lambdas=length(allLambdas); % number of lambda values
+min_LamdaIn = min(allLambdas);
 
-    for f=29:1:length(F)
+% AGL
+% penalty selection and fit precision
+% cd /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization/PCA_32chan_AGL
+addpath(genpath('./AGL_util'));
 
-        fichier =  F(f).name;
+% https://github.com/wodeyara/AdaptiveGraphicalLassoforParCoh
+addpath(genpath('/home/zhibinz2/Documents/GitHub/AdaptiveGraphicalLassoforParCoh'))
 
-        disp(fichier);
-        load(fullfile(F(f).folder,fichier));
+% for d=1%:1:length(DOSSIERS)
+d=1
+
+%     dossier1=DOSSIERS{1,d};
+%     F=dir(fullfile(dossier1,'*.mat'));
+
+    f=29
+
+%     for f=29:1:length(F)
+% 
+%         fichier =  F(f).name;
+% 
+%         disp(fichier);
+%         load(fullfile(F(f).folder,fichier));
 
         % select one trial
         example_data=corti_source_data;
 
-        for Hz=1:1:length(bandlabels)
+        stroke_pcoh5=nan(2,448,448);
+        stroke_Pcov5=nan(2,896,896);
+        stroke_Cov5=nan(2,896,896);
+
+
+        for Hz=5:1:length(bandlabels)
+            display(Hz);
             % pick a frequency to process the data
 
             filtered_data = filter(filt_ds{Hz},example_data);
@@ -50,37 +76,31 @@ for d=1%:1:length(DOSSIERS)
             sourceDataReal = [sourceDataReal*(1/mean(abs(sourceDataReal(:))))]'; % normalize data
             % compute covariance
             stroke_Cov = cov(sourceDataReal);
-            % https://github.com/wodeyara/AdaptiveGraphicalLassoforParCoh
-            addpath(genpath('/home/zhibinz2/Documents/GitHub/AdaptiveGraphicalLassoforParCoh'))
-            % convert real value covariance to complex matrix (448x448)
-            % then compute coherence
-            stroke_coh=normalizeCSD(r2c(stroke_Cov)); % ordinary coherence
+%             stroke_Cov = double(stroke_Cov);
 
-            % set a threshold of 0.4 and convert to binary logical values
-            stroke_coh_binary=zeros(size(stroke_coh));
-            stroke_coh_binary(stroke_coh>0.4)=1;
+%             % convert real value covariance to complex matrix (448x448)
+%             % then compute coherence
+%             stroke_coh=normalizeCSD(r2c(stroke_Cov)); % ordinary coherence
+% 
+%             % set a threshold of 0.4 and convert to binary logical values
+%             stroke_coh_binary=zeros(size(stroke_coh));
+%             stroke_coh_binary(stroke_coh>0.4)=1;
 
-            figure;
-            subplot(121)
-            imagesc(stroke_coh);colorbar
-            title('coherence')
-            subplot(122)
-            imagesc(stroke_coh_binary);colorbar
-            sgtitle('Binary matricies for network analysis')
-            close
+%             figure;
+%             subplot(121)
+%             imagesc(stroke_coh);colorbar
+%             title('coherence')
+%             subplot(122)
+%             imagesc(stroke_coh_binary);colorbar
+%             sgtitle('Binary matricies for network analysis')
+%             close
 
-            Coh.(dossiers{d}).(bandlabels{Hz}).Data(:,:,f)=stroke_coh;
+%             Coh.(dossiers{d}).(bandlabels{Hz}).Data(:,:,f)=stroke_coh;
 
 
             % signficativité de la cohérence
 
-            % AGL variables
-            % https://github.com/wodeyara/AdaptiveGraphicalLassoforParCoh
-           % addpath(genpath('/home/zhibinz2/Documents/GitHub/AdaptiveGraphicalLassoforParCoh'))
-            allLambdas = fliplr([.6,.5,.4,.3,.2,.175,.15,.125, .1, .075, .05, .025, .01]);
-            allLambdasOut = fliplr([.6,.5,.4,.3,.2,.175,.15,.125, .1, .075, .05, .025, .01]);
-            n_Lambdas=length(allLambdas); % number of lambda values
-            min_LamdaIn = min(allLambdas);
+
 
             % split into 2 ensambles: 2 x #source x #(samples/2)
             n_split=2; n_sr=size(sourceDataReal,2);
@@ -91,58 +111,71 @@ for d=1%:1:length(DOSSIERS)
             datapermuted = permute(datareshaped,[2,3,1]); % split into 2 ensambles: 2x896x9300
             % compute covariance
             datapermuted_cov=nan(n_split,n_sr,n_sr);
-            stroke_Cov = cov(sourceDataReal);
+%             stroke_Cov = cov(sourceDataReal);
             for n=1:n_split
                 datapermuted_cov(n,:,:) = cov([squeeze(datapermuted(n,:,:))]');
             end
 
-            % AGL
-            % penalty selection and fit precision
-            % cd /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization/PCA_32chan_AGL
-            addpath(genpath('./AGL_util'));
+
             dataCovs_op=squeeze(datapermuted_cov);
+
+            % debug: check nan
+            sum(isnan(dataCovs_op),'all')
+            % imagesc(squeeze(dataCovs_op(2,:,:)));colorbar
 
             tic
             [penalizationIn_op,penalizationOut_op,minDev_op]=penaltyselection( ...
                 Individual_Connectome,allLambdas,allLambdasOut,dataCovs_op);
             toc
 
-            stroke_Cov=double(stroke_Cov);
+            
+            % debug: check nan
+            sum(isnan(stroke_Cov),'all')
+            stroke_Cov5(Hz,:,:)=stroke_Cov;
 
             tic
             [stroke_Pcov] = fitprecision( ...
                 Individual_Connectome,penalizationIn_op,penalizationOut_op,min_LamdaIn,stroke_Cov);
             toc
 
+            % debug: check nan
+            sum(isnan(stroke_Pcov),'all')
+            stroke_Pcov5(Hz,:,:)=stroke_Pcov;
+
             % add this repo to your path (https://github.com/wodeyara/AdaptiveGraphicalLassoforParCoh)
            % addpath /home/zhibinz2/Documents/GitHub/AdaptiveGraphicalLassoforParCoh/Simulations/util
 
             % convert real value covariance to complex matrix (448x448)
             % then compute coherence
-            stroke_coh=normalizeCSD(r2c(stroke_Cov)); % ordinary coherence
+            stroke_pcoh=normalizeCSD(r2c(stroke_Pcov)); % ordinary coherence
 
-            if sum(sum(isnan(stroke_Pcov))) > 1
-                stroke_Pcoh= NaN(448, 448);
-                ii=ii+1
-                BUG(ii,1)=d;
-                BUG(ii,2)=f;
-                BUG(ii,3)=Hz;
-                disp('bug pour une bande de frequence avec histoire de NaN');
-            else
-            stroke_Pcoh=normalizeCSD(r2c(logical(stroke_Pcov)));% partial coherence using boolean
-            end
-            % SAVE
-          
-            Coh.(dossiers{d}).(bandlabels{Hz}).AGL.Coh(:,:,f)=stroke_coh;
-            Coh.(dossiers{d}).(bandlabels{Hz}).AGL.PCoh(:,:,f)=stroke_Pcoh;
+            % debug: check nan
+            sum(isnan(stroke_pcoh),'all')
+            display(num2str(Hz))
+            stroke_pcoh5(Hz,:,:)=stroke_pcoh;
 
-            clearvars -except ii BUG Coh DOSSIERS dossiers Fs srnew downsample passbands bandlabels attenuation filtered_data Individual_Connectome filt_ds d dossier1 F f fichier example_data Hz
+%             if sum(sum(isnan(stroke_Pcov))) > 1
+%                 stroke_Pcoh= NaN(448, 448);
+%                 ii=ii+1
+%                 BUG(ii,1)=d;
+%                 BUG(ii,2)=f;
+%                 BUG(ii,3)=Hz;
+%                 disp('bug pour une bande de frequence avec histoire de NaN');
+%             else
+%             stroke_Pcoh=normalizeCSD(r2c(logical(stroke_Pcov)));% partial coherence using boolean
+%             end
+%             % SAVE
+%           
+%             Coh.(dossiers{d}).(bandlabels{Hz}).AGL.Coh(:,:,f)=stroke_coh;
+%             Coh.(dossiers{d}).(bandlabels{Hz}).AGL.PCoh(:,:,f)=stroke_Pcoh;
+% 
+%             clearvars -except ii BUG Coh DOSSIERS dossiers Fs srnew downsample passbands bandlabels attenuation filtered_data Individual_Connectome filt_ds d dossier1 F f fichier example_data Hz
 
         end
-    end
-end
+%     end
+% end
 
-save('Coherence_Patient.mat','Coh')% reconstruire BUG pour patient file
+% save('Coherence_Patient.mat','Coh')% reconstruire BUG pour patient file
 
 
-
+save('output_debug.mat','stroke_Pcov5',"stroke_pcoh5",'stroke_Cov5'); % reconstruire BUG pour patient file
---
+++ HNL/ZHIBIN/debugging_forCelia/AGL_nan.m
@@ -0,0 +1,185 @@
+% This is for my own reference: my earlier unorganized code can be found at:
+% open /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization/organize_62stroke/step1_clean_data.m
+% open /home/zhibinz2/Documents/GitHub/EEG_pipeline/HNL/ZHIBIN/EGI_256chan/step2_source_localization_PCA.m
+% open /home/zhibinz2/Documents/GitHub/EEG_pipeline/Celia/Trouble_shooting/wetransfer_calt-mat_2024-09-08_1921/STEP4_Quantification_coherence.m
+% open /home/zhibinz2/Documents/GitHub/EEG_pipeline/HNL/ZHIBIN/AGL_code/AGL_example.m
+% Created by Zhibin 4/6/2024
+% #####################################################################################################################
+
+% This script does the source localization (inverse solution) for the 256 channel EGI raw EEG data
+% And run PCA to aggreate source data in each ROI of the 448 cortical areas
+clear
+%% Construct brain model and create forward matrix in MNE
+% Refer to ../EGI_256chan/MNE_construct_source.ipynb to create the leadfield matrix
+% load forward matrix and source
+load('../base_files/MNE/EGI256/MNE_source_model.mat','leadfield','source_rr');
+% leadfield: leadfield matrix from MNE (256 EEG channels X 5124 brain sources)
+% source_rr: coordinates of the 5124 brain sources
+
+%% Source localization 
+% load the brain model, ROI names and localation files for the 463 ROIs
+load('../base_files/brain_ROIs.mat','Brain','roiNames_250','scale250_subcortROIs','parcels')
+% Brain: The brain model of Lausanne2008_fsaverageDSsurf
+% parcels: contain indices of 463 ROIs in a 3d brain matrix (256x256x256)
+% roiNames_250: labels for 463 ROIs (448 cortical + 15 subcortical ROIs) (scale 250)
+% scale250_subcortROIs: indices for 15 subcortical ROIs in roiNames_250
+
+%% Source localization 
+% load the brain model, ROI names and localation files for the 463 ROIs
+load('../base_files/brain_ROIs.mat','Brain','roiNames_250','scale250_subcortROIs','parcels')
+% Brain: The brain model of Lausanne2008_fsaverageDSsurf
+% parcels: contain indices of 463 ROIs in a 3d brain matrix (256x256x256)
+% roiNames_250: labels for 463 ROIs (448 cortical + 15 subcortical ROIs) (scale 250)
+% scale250_subcortROIs: indices for 15 subcortical ROIs in roiNames_250
+
+% label the sources
+Vertex=Brain.Vertex;
+% Allign the 5124 sources with 463 ROIs and label them
+x_shift=(max(Vertex(:,1))-max(source_rr(:,1))*1e3)/2+(min(Vertex(:,1))-min(source_rr(:,1))*1e3)/2;
+y_shift=(max(Vertex(:,2))-max(source_rr(:,2))*1e3)/2+(min(Vertex(:,2))-min(source_rr(:,2))*1e3)/2;
+z_shift=(max(Vertex(:,3))-max(source_rr(:,3))*1e3)/2+(min(Vertex(:,3))-min(source_rr(:,3))*1e3)/2;
+source_x=source_rr(:,1) * 1e3 + x_shift;
+source_y=source_rr(:,2) * 1e3 + y_shift;
+source_z=source_rr(:,3) * 1e3 + z_shift;
+source_xyz=[source_x source_y source_z];
+num_source=size(source_xyz,1);
+source_fsaverage = source_xyz+127.5; % 127.5 is based on the fsaverage volume being 256 x 256 x 256
+source_labels=zeros(num_source,1);
+for i = 1:length(source_fsaverage)
+    vox = floor(source_fsaverage(i,:)); % change from ceil to floor,now we have 2 subcortical not mapped
+    inds              = sub2ind([size(parcels)], vox(1), vox(2), vox(3));
+    label             = parcels(inds); 
+    source_labels(i) = label;
+end
+% find which ROIs not mapped
+roiNames_250(setdiff(1:463,unique(source_labels))) % 227 457 459
+setdiff(1:463,unique(source_labels))
+
+% get coordinate and label for each ROIs to be aggregated
+ave_source_coor=[];
+ave_source_label=[];
+for sr=1:max(unique(source_labels))
+    I=find(source_labels==sr);
+    if ~isempty(I)
+        ave_source_coor=[ave_source_coor; mean(source_fsaverage(I,:),1)];
+        ave_source_label=[ave_source_label; sr];
+    end
+end 
+
+% create a boolean of subcortical rois
+bool_subcorti=zeros(1,length(ave_source_label));
+for i=1:length(ave_source_label)
+    clear tmp
+    tmp=ave_source_label(i);
+    if ismember(tmp, scale250_subcortROIs)
+        bool_subcorti(i)=1;
+    end
+end
+sum(bool_subcorti)
+ind=find(bool_subcorti);
+ind % use these subcortical indices to remove subcortical aggregated pca data
+
+% remove subcortical ROIs
+ave_source_coor(ind,:)=[];
+ave_source_label(ind)=[];
+% save the cortical ROI coordinates, indicies and labels 
+corti_ave_source_coor=ave_source_coor;
+corti_ave_source_labl=ave_source_label;
+corti_roiNames=roiNames_250(corti_ave_source_labl);
+
+% corti_ave_source_coor: coordinates for 448 cortical ROIs
+% corti_ave_source_labl: indices for 448 cortical ROIs in roiNames_250
+% corti_roiNames: names of the 448 cortical ROIs
+
+% get inverse matrix for source localization
+% You will neded to add the following repo to your path:
+% https://github.com/wodeyara/AdaptiveGraphicalLassoforParCoh/tree/main/Simulations/util
+% addpath(genpath('../../../AdaptiveGraphicalLassoforParCoh/Simulations/util/')); % this is my local path
+cd /home/zhibinz2/Documents/GitHub/AdaptiveGraphicalLassoforParCoh/Simulations/util
+[inversemat] = inversemodel(leadfield,'prctile',1);
+
+%% load Celia's file
+cd /home/zhibinz2/Documents/GitHub/EEG_pipeline/Celia/Trouble_shooting/wetransfer_calt-mat_2024-09-08_1921
+load('CALT.mat','corti_source_data');
+% shorten it for speed
+AGL_example_data=corti_source_data(5000:15000,:);
+
+%% AGL
+cd /home/zhibinz2/Documents/GitHub/EEG_pipeline/HNL/ZHIBIN/AGL_code/
+Fs=1000;
+srnew = 200;
+downsample = Fs/srnew;
+
+passbands = [1 3; 3.5 6.5; 7 10; 10.5 13.5; 14 20; 21 29];
+bandlabels = {'Delta','Theta', 'Alpha', 'Mu', 'Beta1', 'Beta2'};
+
+% make filter
+attenuation=60;
+filt_ds=cell(1,length(passbands));
+for freqBand=1:6
+    % Select frequency
+    passFreq1 = passbands(freqBand,1);
+    passFreq2 = passbands(freqBand,2);
+    filt_d = designfilt('bandpassiir','FilterOrder',20, ...
+    'PassbandFrequency1',passFreq1,'PassbandFrequency2',passFreq2, ...
+    'StopbandAttenuation1',attenuation,'PassbandRipple',0.2, ...
+    'StopbandAttenuation2',attenuation,'SampleRate',srnew);
+    filt_ds{freqBand}=filt_d;
+end
+
+% AGL variables
+% https://github.com/wodeyara/AdaptiveGraphicalLassoforParCoh
+addpath(genpath('/home/zhibinz2/Documents/GitHub/AdaptiveGraphicalLassoforParCoh'))
+allLambdas = fliplr([.6,.5,.4,.3,.2,.175,.15,.125, .1, .075, .05, .025, .01]);
+allLambdasOut = fliplr([.6,.5,.4,.3,.2,.175,.15,.125, .1, .075, .05, .025, .01]);
+n_Lambdas=length(allLambdas); % number of lambda values
+min_LamdaIn = min(allLambdas);
+
+% load individual connectome for AGL
+load("AGL_example_data.mat",'Individual_Connectome')
+
+% pick a frequency to process the data
+freq=6; % beta2
+filtered_data = filter(filt_ds{freq},AGL_example_data);
+hilbertdata = hilbert(filtered_data');
+% combined real and imaginary part
+sourceDataReal = cat(1,real(hilbertdata),imag(hilbertdata));
+sourceDataReal = [sourceDataReal*(1/mean(abs(sourceDataReal(:))))]'; % normalize data
+% split into 2 ensambles: 2 x #source x #(samples/2)
+n_split=2; n_sr=size(sourceDataReal,2);
+sam_len=size(sourceDataReal,1);
+sam_size=floor(sam_len/n_split); sam_range=1:sam_size;
+sourceDataReal = sourceDataReal(1:n_split*sam_size,:);
+datareshaped = reshape(sourceDataReal, sam_size, n_split, n_sr);
+datapermuted = permute(datareshaped,[2,3,1]); % split into 2 ensambles: 2x896x9300
+% compute covariance
+datapermuted_cov=nan(n_split,n_sr,n_sr);
+stroke_Cov = cov(sourceDataReal);
+for n=1:n_split
+    datapermuted_cov(n,:,:) = cov([squeeze(datapermuted(n,:,:))]');
+end
+
+% AGL
+% penalty selection and fit precision
+% cd /home/zhibinz2/Documents/GitHub/MEG_EEG_Source_Localization/PCA_32chan_AGL
+addpath(genpath('./AGL_util'));
+dataCovs_op=squeeze(datapermuted_cov);
+tic
+[penalizationIn_op,penalizationOut_op,minDev_op]=penaltyselection( ...
+Individual_Connectome,allLambdas,allLambdasOut,dataCovs_op);
+toc % take 701 seconds for this example
+
+tic
+[stroke_Pcov] = fitprecision( ...
+Individual_Connectome,penalizationIn_op,penalizationOut_op,min_LamdaIn,stroke_Cov);
+toc % take 277 seconds for this example
+
+% add this repo to your path (https://github.com/wodeyara/AdaptiveGraphicalLassoforParCoh)
+addpath /home/zhibinz2/Documents/GitHub/AdaptiveGraphicalLassoforParCoh/Simulations/util
+
+% convert real value covariance to complex matrix (448x448) 
+% then compute coherence
+stroke_coh=normalizeCSD(r2c(stroke_Cov)); % ordinary coherence
+stroke_Pcoh=normalizeCSD(r2c(logical(stroke_Pcov)));% partial coherence using boolean
+
+sum(isnan(stroke_pcoh),'all')
